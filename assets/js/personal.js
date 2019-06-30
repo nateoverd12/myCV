343abc444d585fd1674aacf6cba76631314f5251
root <root@vps702344.ovh.net>
2019-06-30 19:47:32 +0200

    increased slider speed

--- assets/js/personal.js
+++ assets/js/personal.js
@@ -8,7 +8,7 @@ var swiper = new Swiper('.swiper-container', {
   // effect: 'fade',
   loop: true,
   autoplay:{
-    delay:300,
+    delay:240,
     autoplayDisableOnInteraction: true
   },
   keyboard: {
