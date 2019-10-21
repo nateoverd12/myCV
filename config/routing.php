1d7877a1b814824f5137f2fd72ff49fe0858b92c
nateover <jonath355@sfr.fr>
2019-10-21 23:37:30 +0200

    able to difference whether pdf with photo

--- config/routing.php
+++ config/routing.php
@@ -9,17 +9,20 @@
  * @link     https://github.com/WildCodeSchool/simple-mvc
  */
 
+ // try to get geoloc of user to guess his country and then display accurated flag
 $response = file_get_contents("http://ip-api.com/json");
 $response = json_decode($response);
 $cc = $response->countryCode;
+
+$photoInPDF = !in_array($cc, ['US', 'CA']);
 
 $routeParts = explode('/', ltrim($_SERVER['PHP_SELF'], '/'));
 array_shift($routeParts);
 
 if($routeParts==['fr']){
-    echo $twig->render('idioms/french.twig', ['cc' => $cc]);
+    echo $twig->render('idioms/french.twig', ['cc' => $cc, 'photoInPDF' => $photoInPDF]);
 } elseif (empty($routeParts)){
-    echo $twig->render('idioms/english.twig');
+    echo $twig->render('idioms/english.twig', ['cc' => $cc, 'photoInPDF' => $photoInPDF]);
 } else {
     // header("HTTP/1.0 404 Not Found");
     echo '404 - Page not found';
